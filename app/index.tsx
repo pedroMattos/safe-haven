5e90621a7ce705eb10a6e7b207c514d7b7f9fe99
Pedro Mattos <laranjeiraspedro@gmail.com>
2025-01-08 22:24:22 -0300

    feature: add safe area and list flat

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,32 @@
-import { View, Text, StyleSheet, Button, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Button, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/Colors'
 import { useColorScheme } from '@/hooks/useColorScheme.web';
+import { SimpleList } from '@/moks/ListOfSecrets'
+import { Secret } from '@/components/Secret';
 
 const app = () => {
   const colorScheme = useColorScheme();
 
   return (
     <View style={styles.container}>
-      <Text style={{ color: Colors[colorScheme ?? 'light'].text, ...styles.text }}>Nenhum arquivo salvo</Text>
-      <Text style={{ color: Colors[colorScheme ?? 'light'].text, ...styles.tinyText }}>Para criar um novo arquivo clique no botão abaixo</Text>
-      <Button
-        title='Adicionar novo item'
-      />
+      <SafeAreaView>
+        <Text style={styles.text}>Nenhum arquivo salvo</Text>
+        <Text style={styles.tinyText}>Para criar um novo arquivo clique no botão abaixo</Text>
+
+        <FlatList
+          data={SimpleList}
+          keyExtractor={(item) => item.id.toString()}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.contentContainer}
+          renderItem={({ item }) => (
+            <Secret {...item} />
+          )}
+        />
+        <Button
+          title='Adicionar novo item'
+        />
+      </SafeAreaView>
     </View>
   )
 }
@@ -24,7 +38,13 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    padding: 10
+    padding: 10,
+    backgroundColor: 'white'
+  },
+  contentContainer: {
+    paddingTop: 10,
+    paddingBottom: 20,
+    paddingHorizontal: 12
   },
   text: {
     fontSize: 24,
