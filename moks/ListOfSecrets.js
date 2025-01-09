8823b2fea1cf4cedd36b56c2938812725b18e1cc
Pedro Mattos <laranjeiraspedro@gmail.com>
2025-01-08 22:25:00 -0300

    feat: mock

---
+++ moks/ListOfSecrets.js
@@ -0,0 +1,10 @@
+const SimpleList = [
+  { name: 'teste', id: 1, date: '2023-06-03', password: null, isProtected: false  },
+  { name: 'teste 2', id: 2, date: '2023-06-03', password: '12345678', isProtected: false },
+  { name: 'teste 3', id: 3, date: '2023-06-03', password: null, isProtected: true },
+  { name: 'teste 3', id: 4, date: '2023-06-03', password: '12345678', isProtected: true },
+]
+
+export {
+  SimpleList
+}
